9ff805051eea54c73372a517f7c2c6e29e5baca6
natvqgit <nataliaquintero706@gmail.com>
2026-03-26 17:14:53 -0700

    agregacion de models

--- blog/models.py
+++ blog/models.py
@@ -1,27 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
+
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
+
 
     class Meta:
         ordering = ('title',)
         verbose_name_plural = 'Categories'
 
+
     def __str__(self):
         return self.title
 
 
+
+
 class Post(models.Model):
+
 
     ACTIVE = 'active'
     DRAFT = 'draft'
+
 
     CHOICES_STATUS = [
         (ACTIVE, 'Active'),
         (DRAFT, 'Draft')
     ]
+
 
     category = models.ForeignKey(Category, related_name='post', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
@@ -31,10 +40,13 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
     image = models.ImageField(upload_to='upload/', blank=True, null=True)
 
+
     def __str__(self):
         return self.title
 
 
+
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
@@ -42,19 +54,24 @@ class Comment(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+
     def __str__(self):
         return self.name
-    
+   
         def get_absolute_url(self):
          return '/%s/%s/' % (self.category.slug, self.slug)
 
 
+
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.EmailField()
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
 
 
     def __str__(self):

607a54a4073b25d011dc115c58d9b16c5bf6461c
natvqgit <nataliaquintero706@gmail.com>
2026-03-26 17:11:11 -0700

    agregacion de admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Category, Comment, Post
 
+
+
+
 class CommentItemInline(admin.TabularInline):
     model = Comment
     raw_id_fields = ['post']
+
+
+
 
 class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'intro', 'body']
@@ -11,18 +17,30 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ['category', 'created_at', 'status']
     inlines = [CommentItemInline]
 
+
+
+
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['title', 'intro', 'body']
     list_display = ('title', 'category', 'created at', 'status')
     list_filter = ['category', 'created at', 'status']
     inlines = [CommentItemInline]
 
+
+
+
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['title']
     list_display = ('title', )
 
+
+
+
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'post', 'created_at', )
+
+
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
